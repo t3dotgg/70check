5ad726a210ddaa6b86ad6e089c2208449b2c2925
Brandon (opti) <40129778+opti21@users.noreply.github.com>
2023-06-15 23:26:58 -0500

    cleanup and layout fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import {
   Card,
   CardContent,
@@ -14,7 +14,7 @@ const UploadCSVButton = (props: { onUpload: (csv: string[][]) => void }) => {
   return (
     <div className="flex flex-col items-center justify-center">
       <Button>
-        <Label htmlFor="upload-csv" className="hover:cursor-pointer" >
+        <Label htmlFor="upload-csv" className="hover:cursor-pointer">
           Upload <code>subscriber-list.csv</code>
         </Label>
       </Button>
@@ -52,35 +52,40 @@ const Content = () => {
   if (csv) {
     if (calculation > 350)
       return (
-        <div className="text-xl">{`You're eligible! You have ${calculation} recurring subs`}</div>
+        <div className="text-xl text-center">
+        {`You're eligible! 🥳 `} <br />
+        {`you have ${calculation} recurring subs`} <br />
+        </div>
       );
 
     return (
-      <div className="text-xl">{`You're not eligible :( you have ${calculation} recurring subs, which is ${
-        350 - calculation
-      } away`}</div>
+      <div className="text-xl text-center">
+        {`You're not eligible 😔 `} <br />
+        {`You have ${calculation} recurring subs,`} <br />
+        {` which is ${
+          350 - calculation
+        } away`}
+      </div>
     );
   }
 
   return (
     <div>
-      <h2 className="text-xl">
-        Step 1: Go to the{" "}
-        <a
-          href="https://dashboard.twitch.tv/analytics/revenue-earnings/subscriptions"
-          target="_blank"
-          className="text-blue-400 underline"
-        >
-          subscriptions revenue page in the Twitch dashboard
-        </a>
-      </h2>
-      <h2 className="text-xl">
-        {
-          "Step 2: Click `Download my subscribers list` (on the right under Subscribers)"
-        }
-      </h2>
-      <h2 className="text-xl">{"Step 3: Upload the file below"}</h2>
-      <div className="text-gray-200 font-light italic">
+      <ol className="list-decimal list-inside mb-4">
+        <li>
+          {"Download the CSV from "}
+          <a
+            href="https://dashboard.twitch.tv/analytics/revenue-earnings/subscriptions"
+            target="_blank"
+            className="text-blue-400 underline"
+          >
+            {"the Twitch dashboard"}
+          </a>
+        </li>
+        <li>{"Upload the CSV below"}</li>
+        <li>{"See if you're eligible!"}</li>
+      </ol>
+      <div className="text-gray-200 font-light italic text-xs">
         {"(Your data never leaves your computer - "}
         <a
           href="https://github.com/t3dotgg/70check"
@@ -99,41 +104,42 @@ const Content = () => {
 };
 
 export default function Home() {
-
   return (
-    <main className="container flex items-center justify-center min-h-screen font-sans">
+    <main className="container flex items-center justify-center min-h-screen font-sans max-w-[550px]">
       <div>
-        <Card className="max-[550px]">
-          <CardHeader>
-            <CardTitle className="text-4xl text-center">70Check</CardTitle>
-          </CardHeader>
-          <CardContent>
+        <div>
+          <div className="flex flex-col items-center justify-center mb-4">
+            <h1 className="text-5xl font-bold mb-4">70Check</h1>
             <div className="mb-4 text-center">
               {"A quick way to check if you're eligible for "}
+              <br />
               <a
                 href="https://blog.twitch.tv/en/2023/06/15/introducing-the-partner-plus-program/"
-          target="_blank"
+                target="_blank"
                 className="text-blue-400 underline"
               >
                 {'Twitch\'s new 70/30 split "Partner Plus" Program'}
               </a>
               <br />
-        <div className="text-gray-200 font-light italic">
+              <div className="text-gray-200 font-light italic">
                 {"(Quickly made by "}
                 <a
                   href="https://twitter.com/t3dotgg"
-            target="_blank"
+                  target="_blank"
                   className="text-blue-400 underline"
                 >
                   {"Theo"}
                 </a>
                 {")"}
               </div>
-      </div>
+            </div>
+          </div>
+        </div>
+        <Card>
+          <CardContent className="mt-5 h-[210px] max-w-[430px] sm:min-w-[430px] flex items-center justify-center">
             <Content />
           </CardContent>
         </Card>
-
       </div>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
       },
     },
     extend: {
+      transitionProperty:{
+        'height': 'height',
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -27,26 +30,6 @@ module.exports = {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
         },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
       },
       borderRadius: {
         lg: `var(--radius)`,
@@ -55,20 +38,6 @@ module.exports = {
       },
       fontFamily: {
         sans: ['var(--font-inter)', ...fontFamily.sans],
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
